e2eb14166466427589ab78407213b22347c9d737
Marti Merisalu <marti.merisalu@tktk.ee>
2023-05-07 14:06:15 +0300

    create new app

---
+++ Esimene/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//nimepidi tervitav rakendus
+
+
+Console.WriteLine("Enter your name"); //output
+//string - Sõne(sõnad ainult)
+string userName = Console.ReadLine();  //input
+
+// string interpolation
+Console.WriteLine($"Hello, {userName} !"); //output
